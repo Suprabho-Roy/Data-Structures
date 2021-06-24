9fc2dd5e5e6c774a6a6ac0b14a703b7972b506be
Suprabho Roy <roysuprabho12@gmail.com>
2021-06-24 22:29:44 +0530

    added maps and sets

---
+++ Hash/maps&Sets.js
@@ -0,0 +1,11 @@
+const a = new Map();
+
+// with maps we can save any data type as keys.
+// (functions of arrays also)
+// with objects, that's not possible
+// Map() maintains insertion order. So that we can
+// loop through
+
+const b = new Set();
+
+// Sets only stores the keys. No values.
