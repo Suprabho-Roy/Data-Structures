e97802bbdf79dead6d2609f6aca15907c5240e48
Suprabho Roy <roysuprabho12@gmail.com>
2021-06-26 01:28:26 +0530

    changes

--- Hash/hashimplement.js
+++ Hash/hashimplement.js
@@ -1,1 +1,48 @@
-const later = 'will do tomorrow';
+class HashTable{
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+    _hash(key){
+        let hash = 0;
+        for(let i=0; i<key.length; i++){
+            hash = (hash + key.charCodeAt(i)*i) % this.data.length;
+        }
+
+        return hash;
+    }
+
+    set(key, value){
+        let address = this._hash(key);
+        if(!this.data[address]){
+            this.data[address] = [];
+        }
+        
+        this.data[address].push([key, value]);
+        console.log(this.data);
+        
+    }
+
+    get(key){
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+
+        if(currentBucket){
+            for(let i = 0; i < currentBucket.length; i++){
+                if(currentBucket[i][0] === key){
+                    console.log(currentBucket[i][1]);
+                }
+            }
+        }else{
+            console.log('undefined');
+        }
+    }
+}
+
+const myHashTable = new HashTable(2);
+
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 20);
+
+myHashTable.get('grapes');
+myHashTable.get('apples');
