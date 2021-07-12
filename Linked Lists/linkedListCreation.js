38426f743fc9a50ebb2ba18134c2b0e50201fee1
Suprabho Roy <roysuprabho12@gmail.com>
2021-07-13 00:27:37 +0530

    major changes

--- Linked Lists/linkedListCreation.js
+++ Linked Lists/linkedListCreation.js
@@ -27,13 +27,77 @@ class LinkedList{
         const newNode = {
             value: value,
             next: null
-        }
+        };
 
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
+    }
 
-        console.log(this);
+    prepend(value){
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        newNode.next = this.head;
+
+        this.head = newNode;
+        this.length++;
+    }
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode != null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        console.log(array);
+    }
+
+    insert(index, value){
+        if(index >= this.length){
+            this.append(value);
+        }
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        const leader = this.traverseToIndex(index-1);
+
+        const holdingPointer = leader.next;
+
+        leader.next = newNode;
+
+        newNode.next = holdingPointer;
+
+        this.length++;
+
+    }
+
+    traverseToIndex(index){
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+
+    remove(index){
+        const leader = this.traverseToIndex(index-1);
+        let nodeToDelete = leader.next;
+        const nextNode = nodeToDelete.next;
+
+        leader.next = nextNode;
+
+        this.length--;
     }
 }
 
@@ -42,4 +106,18 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 
-//console.log(myLinkedList); 
+myLinkedList.prepend(1);
+
+myLinkedList.printList();
+
+myLinkedList.insert(2, 99);
+
+myLinkedList.printList();
+
+myLinkedList.remove(3);
+
+myLinkedList.printList();
+
+
+
+//console.log(myLinkedList.head);
