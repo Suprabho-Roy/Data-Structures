09923e56c8bd0db335273bb466aefeb373efa9a7
Suprabho Roy <roysuprabho12@gmail.com>
2021-07-20 01:25:14 +0530

    javascript concept asynchronous added

---
+++ Stacks and Queues/Asynchronous.js
@@ -0,0 +1,12 @@
+
+
+console.log('1');
+
+
+setTimeout(() => {
+    console.log('2');
+}, 2000)              //This how we use asynchronous
+// programming for bigger functions in the callstack
+// to get rid of waiting around
+
+console.log('3');
