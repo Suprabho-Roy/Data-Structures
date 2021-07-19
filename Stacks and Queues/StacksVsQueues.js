eac7ea06446d615e1f9d10916e4001d7948b3035
Suprabho Roy <roysuprabho12@gmail.com>
2021-07-19 23:47:27 +0530

    added stacks

---
+++ Stacks and Queues/StacksVsQueues.js
@@ -0,0 +1,20 @@
+//Stacks
+
+//google
+//udemy.com
+//youtube
+
+//with Arrays or Linked Lists (we can use both)
+
+
+
+//Queues
+
+// Lou -- Jake -- John -- Charlie
+
+// Arrays or Linked Lists
+
+// In Queue, we don't use Arrays cause the element
+// from the first of the Queue will be removed, so
+// everytime we pop() an element we will have to
+// shift the indexes. This will take time.
