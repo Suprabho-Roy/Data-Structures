85d933fa5e20d890058302a0fc478d13f62f177b
Suprabho Roy <roysuprabho12@gmail.com>
2021-07-30 01:51:02 +0530

    Tree added

---
+++ Trees/O(logn).js
@@ -0,0 +1,13 @@
+// Level 0: 2^0 = 1;
+// Level 1: 2^1 = 2;
+// Level 2: 2^2 = 4;
+// Level 3: 2^3 = 8;
+
+// Total # of Nodes = 2^h - 1; (h=height);
+
+// log Nodes = steps;
+
+// The Number of iterations the Tree will take to 
+// find a node based on the height is log(Nodes);
+
+// O(log n) is much faster than O(n);
